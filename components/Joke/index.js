006ac39c3d21717710f70458fac29a88f6537c2f
Dominik Hautau <dominikhautau@gmail.com>
2023-05-05 10:16:28 +0200

    react data fetching session

--- components/Joke/index.js
+++ components/Joke/index.js
@@ -1,48 +1,41 @@
-import { useEffect, useState } from "react";
-
-function useFetch(url) {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    async function startFetching() {
-      const response = await fetch(url);
-      const newData = await response.json();
-
-      setData(newData);
-    }
-
-    startFetching();
-  }, [url]);
-
-  return data;
-}
+import { useState } from "react";
+import useSWR from "swr";
 
 export default function Joke() {
   const [id, setId] = useState(0);
+  const { data, isLoading } = useSWR(
+    `https://example-apis.vercel.app/api/bad-jokes/${id}`
+  );
 
-  const data = useFetch(`https://example-apis.vercel.app/api/bad-jokes/${id}`);
+  const [funnyJokes, setFunnyJokes] = useState([0, 2, 4]);
+  const funnyJoke = funnyJokes.includes(id);
 
-  function handlePrevJoke() {
-    setId(data.prevId);
-  }
-
-  function handleNextJoke() {
-    setId(data.nextId);
-  }
-
-  if (!data) {
+  if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
   return (
     <>
       <small>ID: {id}</small>
-      <h1>{data.joke}</h1>
+      <h1>
+        {data.joke} - {funnyJoke ? "funny" : "unfunny"}
+      </h1>
+      <button
+        onClick={() => {
+          if (funnyJoke) {
+            setFunnyJokes(funnyJokes.filter((jokeId) => jokeId !== id));
+          } else {
+            setFunnyJokes([...funnyJokes, id]);
+          }
+        }}
+      >
+        is funny
+      </button>
       <div>
-        <button type="button" onClick={handlePrevJoke}>
+        <button type="button" onClick={() => setId(data.prevId)}>
           ← Prev Joke
         </button>
-        <button type="button" onClick={handleNextJoke}>
+        <button type="button" onClick={() => setId(data.nextId)}>
           Next Joke →
         </button>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SWRConfig
+      value={{ fetcher: (...args) => fetch(...args).then((res) => res.json()) }}
+    >
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
